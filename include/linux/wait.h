ba1067047aa9bd9776c1bd3369974030346d238c
enlin.mu <2277901360@qq.com>
2019-10-07 18:47:04 +0800

    add info for wait_queue

--- include/linux/wait.h
+++ include/linux/wait.h
@@ -31,7 +31,7 @@ typedef int (*wait_queue_func_t)(wait_queue_t *wait, unsigned mode, int sync, vo
 int default_wake_function(wait_queue_t *wait, unsigned mode, int sync, void *key);
 
 struct __wait_queue {
-	unsigned int flags;
+	unsigned int flags;//0:非互斥进程，总是唤醒；1:互斥进程，选择性唤醒
 #define WQ_FLAG_EXCLUSIVE	0x01
 	struct task_struct * task;
 	wait_queue_func_t func;
